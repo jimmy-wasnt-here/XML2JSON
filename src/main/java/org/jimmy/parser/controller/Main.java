7283d1da8d855f4cf533283037aaec4a0c7c9a2a
Jaime <jaime.t.velasquez@gmail.com>
2018-09-06 22:06:40 +0200

    Optimize imports

--- src/main/java/org/jimmy/parser/controller/Main.java
+++ src/main/java/org/jimmy/parser/controller/Main.java
@@ -1,10 +1,5 @@
 package org.jimmy.parser.controller;
 
-import org.jimmy.parser.view.Principal;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.XML;
-
 public class Main {
     public static void main(String[] args) {
         new PrincipalController();
